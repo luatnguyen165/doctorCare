5f71f20995add93db09811671dbd11e7786f8835
luatnguyen165 <42195286+luatnguyen165@users.noreply.github.com>
2021-09-25 14:23:40 +0700

    update docker (#10)
    Co-authored-by: luatnguyen165 <luatnguyen165@.com>

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,6 @@
 const Users = require("../models/Users");
 const nodemailer = require("nodemailer");
-
+const jwt = require('jsonwebtoken');
 module.exports ={
     getListRegister: async function(req, res, next) {
         const userList = await Users.find();
@@ -9,34 +9,61 @@ module.exports ={
     getRegister: async function(req, res, next) {
         res.render('register')
     },
+    // getVerify: async function(req, res, next) {
+    //     const token = req.params.token;
+    //     res.redirect(`/verify/${token}`)
+    // },
+    postVerify: async function(req, res, next) {
+        const token = req.params.token;
+        const user = await Users.find({tokenMail:token})
+        if(user.length>0){
+            const id = user[0]._id;
+            const item =  await Users.findByIdAndUpdate(id,{
+                verify:true,
+                tokenMail:null
+            })
+            await item.save();
+        }
+        user.length>0?res.send('chứng thực token'):res.send('chứng thực failed');
+    },
+
     postRegister: async function(req, res, next) {
         try{
             const { firstName,lastName,email,password,phone }=req.body;
-            const user=new Users({firstName,lastName,email,password,phone});
-            
+            const token = jwt.sign({email:email},process.env.TOKENMAIL,{
+                expiresIn:process.env.EXPRIREMAIL
+            })
+            const tokenMail=token;
+            const user=new Users({firstName,lastName,email,password,phone,tokenMail});
+            const item = await user.save();
             try{
                 var mainOptions = { // thiết lập đối tượng, nội dung gửi mail
                     from: 'Doctor Care',
                     to: email,
                     subject: 'Verify Account',
-                    text: 'You recieved message from ' + email,
-                    html: `<p>You have got a new message</b><ul><li>Username:' + lastName + firstName  + '</li><li>Email:' + email + '</li></ul></p> <a href="http://localhost:3000/verify/${token}">`
+                    text: `You recieved message from ${email}`,
+                    html: `<p>You have got a new message</b><ul><li>Username:' ${lastName}  ${firstName} '</li><li>Email:' ${email} + '</li></ul></p> <a href="http://localhost:3000/verify/${token}">Verify Account</a>`
                 }
                 let transporter = nodemailer.createTransport({
                     host: "smtp.gmail.com",
-                    port: 587,
-                    secure: false, // true for 465, false for other ports
+                    port: 465,
+                    secure: true, // true for 465, false for other ports
                     auth: {
-                      user: testAccount.user, // generated ethereal user
-                      pass: testAccount.pass, // generated ethereal password
+                      user: process.env.USER_MAIL, // generated ethereal user
+                      pass:process.env.PASS_MAIL, // generated ethereal password
                     },
                 });
-
-                
+                transporter.sendMail(mainOptions, function(err, info){
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        console.log('Message sent: ' +  info.response);
+                    }
+                });
             }catch(e){
                 throw new Error(e)
             }
-            const item = await user.save();
+            
             item?res.status(200).json({
                 message:'Đăng ký thành công',
                 userLists:item,
