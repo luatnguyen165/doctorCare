03610566821ee9ff98853f35599e71eef50cec92
luatnguyen165 <luatnguyen165@.com>
2021-09-27 00:36:01 +0700

    example full-text-search

--- app.js
+++ app.js
@@ -18,6 +18,61 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+// Phần này là của elasticsearch  và ví dụ
+// học link này full text search nâng cao
+// https://www.elastic.co/guide/en/elasticsearch//reference/current/query-dsl-multi-match-query.html
+const { Client } = require('@elastic/elasticsearch')
+const client = new Client({ node: 'http://elasticsearch:9200' })
+async function run () {
+  // Let's start by indexing some data
+  await client.index({
+    index: 'game-of-thrones',
+    // type: '_doc', // uncomment this line if you are using Elasticsearch ≤ 6
+    body: {
+      character: 'Ned Stark',
+      quote: 'Winter is coming.'
+    }
+  })
+
+  await client.index({
+    index: 'game-of-thrones',
+    // type: '_doc', // uncomment this line if you are using Elasticsearch ≤ 6
+    body: {
+      character: 'Daenerys Targaryen',
+      quote: 'I am Winter the blood of the dragon.'
+    }
+  })
+
+  await client.index({
+    index: 'game-of-thrones',
+    // type: '_doc', // uncomment this line if you are using Elasticsearch ≤ 6
+    body: {
+      character: 'Tyrion Lannister',
+      quote: 'A mind needs books like a sword needs a whetstone.'
+    }
+  })
+
+  // here we are forcing an index refresh, otherwise we will not
+  // get any result in the consequent search
+  await client.indices.refresh({ index: 'game-of-thrones' })
+
+  // Let's search!
+  const { body } = await client.search({
+    index: 'game-of-thrones',
+    // type: '_doc', // uncomment this line if you are using Elasticsearch ≤ 6
+    body: {
+      query: {
+        match: { quote: 'winter' }
+      }
+    }
+  })
+
+  console.log(body.hits.hits)
+}
+
+run().catch(console.log)
+// end elasticsearch
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
