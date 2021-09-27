134ece46ec2b55771882c8500167048ca95c8d12
congdanh <congdanh170708@gmail.com>
2021-09-27 23:57:08 +0700

    update

--- models/specialists.js
+++ models/specialists.js
@@ -1,1 +1,19 @@
 const mongoose = require('mongoose');
+
+const specialistSchema = mongoose.Schema({
+    Name:{
+        type:String
+    },
+    lastName:{
+        type:String
+    },
+    email:{
+        type:String
+    },
+    password:{
+        type:String
+    },
+    phone:{
+        type:String
+    },
+}
